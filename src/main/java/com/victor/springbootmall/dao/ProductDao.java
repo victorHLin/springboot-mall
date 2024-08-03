dbff955cb8d73f88eb23655bd2b20afdb3ee38e9
victorHLin <lin1129@alumni.upenn.edu>
2024-08-03 10:15:36 -0500

    Finish get all products feature

--- src/main/java/com/victor/springbootmall/dao/ProductDao.java
+++ src/main/java/com/victor/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.victor.springbootmall.dao;
 import com.victor.springbootmall.dto.ProductRequest;
 import com.victor.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/victor/springbootmall/service/ProductService.java
+++ src/main/java/com/victor/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.victor.springbootmall.service;
 import com.victor.springbootmall.dto.ProductRequest;
 import com.victor.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
+
     Product getProductById(int id);
 
     Integer createProduct(ProductRequest productRequest);
