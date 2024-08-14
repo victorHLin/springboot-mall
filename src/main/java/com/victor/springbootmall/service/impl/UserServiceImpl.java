aa0bd9d7b642cf3f463c9d694f930bc85e8483b1
victorHLin <lin1129@alumni.upenn.edu>
2024-08-13 20:26:41 -0500

    Enhance password storing with md5 hash

--- src/main/java/com/victor/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/victor/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -28,6 +29,9 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         return userDao.createUser(userRegisterRequest);
     }
 
@@ -44,8 +48,9 @@ public class UserServiceImpl implements UserService {
             log.warn("Account with email {} not exist", userLoginRequest.getEmail())  ;
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
 
-        if(!user.getPassword().equals(userLoginRequest.getPassword())){
+        if(!user.getPassword().equals(hashedPassword)){
             log.warn("Email {} doesn't match the password", userLoginRequest.getEmail())  ;
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
