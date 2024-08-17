1aa2f628970f1cb6047b2af458ca2d85f94beb6c
victorHLin <lin1129@alumni.upenn.edu>
2024-08-17 11:54:50 -0500

    Finish orders unit tests and fix some bugs

--- src/main/java/com/victor/springbootmall/controller/OrderController.java
+++ src/main/java/com/victor/springbootmall/controller/OrderController.java
@@ -11,9 +11,11 @@ import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import  java.util.List;
 
+@Validated
 @RestController
 public class OrderController {
     @Autowired
